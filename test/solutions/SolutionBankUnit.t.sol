98dd1b0ff6e1ecab7d7980b0cf71e1c8a96208f7
kittikornl <pkorn003@gmail.com>
2024-07-15 13:22:57 +0700

    Add solutions

---
+++ test/solutions/SolutionBankUnit.t.sol
@@ -0,0 +1,199 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Bank} from "../../src/Bank.sol";
+import {MockERC20} from "../../src/MockERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract SolutionBankUnitTest is Test {
+    Bank bank;
+
+    address token;
+    address usdt = address(0);
+    address ALICE = address(0xA11CE);
+    address BOB = address(0xB0B);
+    address OWNER = address(0xBEEF);
+
+    // note: to run test
+    // forge test -vv --mc TestContract --mt TestFunction
+    function setUp() public {
+        // note: all tests will be run through this setup first
+        token = address(new MockERC20());
+        vm.startPrank(OWNER);
+        bank = new Bank(token);
+        bank.open();
+        vm.stopPrank();
+    }
+
+    // =========================== unit test ===========================
+    // forge test -vv --mc BankUnitTest --mt test_ownerOpen
+    // try -vvvv to see the call trace
+    function test_ownerOpen() public {
+        vm.startPrank(OWNER);
+        bank.open();
+        vm.stopPrank();
+        assertTrue(bank.isOpened(), "not opened");
+        assertEq(bank.OWNER(), OWNER, "invalid owner");
+    }
+
+    function test_notOwnerOpen() public {
+        // note: check that the next contract call will be reverted
+        vm.expectRevert("not owner");
+        bank.open();
+    }
+
+    // imposonate as owner to test close function
+    function test_ownerClose() public {
+        // todo: impersonate
+        vm.startPrank(OWNER);
+        bank.close();
+        vm.stopPrank();
+        // todo: do we need to check the state of the contract?
+        assertFalse(bank.isOpened(), "not closed");
+        assertEq(bank.OWNER(), OWNER, "invalid owner");
+    }
+
+    function test_notOwnerClose() public {
+        // todo: test me
+    }
+
+    function test_dealETH() public {
+        uint256 amount = 1e18;
+        console.log("balBf", ALICE.balance);
+        deal(ALICE, amount);
+        console.log("balAf", ALICE.balance);
+    }
+
+    function test_dealERC20() public {
+        uint256 amount = 1e18;
+        console.log("balBf", IERC20(token).balanceOf(ALICE));
+        deal(token, ALICE, amount);
+        console.log("balAf", IERC20(token).balanceOf(ALICE));
+    }
+
+    function test_fakeTimestamp() public {
+        console.log("t1", block.timestamp);
+        skip(1 days);
+        console.log("t2", block.timestamp);
+        vm.warp(100);
+        console.log("t3", block.timestamp);
+    }
+
+    function test_snapShot() public {
+        console.log("t1", block.timestamp);
+        uint256 snapShotId = vm.snapshot();
+        skip(1 days);
+        console.log("t2", block.timestamp);
+        vm.revertTo(snapShotId);
+        console.log("revert to t1");
+        console.log("t3", block.timestamp);
+    }
+
+    function test_depositSimple() public {
+        uint256 amount = 1e18;
+        // add token to alice
+        deal(token, ALICE, amount);
+        // load state action for testing
+        uint256 aliceBalBf = IERC20(token).balanceOf(ALICE);
+        uint256 bankBalBf = IERC20(token).balanceOf(address(bank));
+        uint256 aliceBankBalBf = bank.balances(ALICE);
+        // prank alice
+        vm.startPrank(ALICE);
+        // approve token for bank to be able to transfer token from alice
+        IERC20(token).approve(address(bank), amount);
+        bank.deposit(amount);
+        vm.stopPrank();
+        // check the balance of bank is correct
+        assertEq(IERC20(token).balanceOf(address(bank)) - bankBalBf, amount, "invalid bank balance");
+        // check the balance of alice is correct
+        assertEq(aliceBalBf - IERC20(token).balanceOf(ALICE), amount, "invalid alice balance");
+        // check the alice's balance in bank is correct
+        assertEq(bank.balances(ALICE) - aliceBankBalBf, amount, "invalid bank balance");
+        // check the last deposit time of alice is correct
+        assertEq(bank.lastDepositTimestamps(ALICE), block.timestamp, "invalid last deposit time");
+        // note: check StdAssertions to see more assert for testing
+        // ex:
+        // - assertApproxEqAbs => absolute diff
+        // - assertApproxEqRel => percentage diff
+    }
+
+    function test_depositZero() public {
+        // try: console.log in Bank.sol to see where the revert happens
+        vm.expectRevert();
+        bank.deposit(0);
+    }
+
+    function test_depositExceedMaxBalance() public {
+        uint256 amount = bank.maxBalance() - IERC20(token).balanceOf(address(bank)) + 1;
+        // add token to alice
+        deal(token, ALICE, amount);
+        vm.startPrank(ALICE);
+        IERC20(token).approve(address(bank), amount);
+        vm.expectRevert("exceed max balance");
+        bank.deposit(amount);
+        vm.stopPrank();
+    }
+
+    function test_withdrawSimple() public {
+        uint256 amount = 1e18;
+        // todo: add token to alice
+        deal(token, ALICE, amount);
+        vm.startPrank(ALICE);
+        // approve token for bank to be able to transfer token from alice
+        IERC20(token).approve(address(bank), amount);
+        bank.deposit(amount);
+        vm.stopPrank();
+        // todo: time travel
+        skip(1 days);
+        // load state action for testing
+        uint256 aliceBalBf = IERC20(token).balanceOf(ALICE);
+        // todo: load more states for testing
+        uint256 bankBalBf = IERC20(token).balanceOf(address(bank));
+        uint256 aliceBankBalBf = bank.balances(ALICE);
+        // prank alice
+        vm.startPrank(ALICE);
+        bank.withdraw(amount);
+        vm.stopPrank();
+        // check the balance of alice is correct
+        assertEq(IERC20(token).balanceOf(ALICE) - aliceBalBf, amount, "invalid alice balance");
+        // todo: add more asserts
+        assertEq(bankBalBf - IERC20(token).balanceOf(address(bank)), amount, "invalid bank balance");
+        assertEq(aliceBankBalBf - bank.balances(ALICE), amount, "invalid bank balance");
+    }
+
+    function test_withdrawZero() public {
+        // todo: test me
+        vm.expectRevert("zero withdraw");
+        bank.withdraw(0);
+    }
+
+    function test_withdrawInsufficientBalance() public {
+        // todo: test me
+        uint256 amount = 1e18;
+        // add token to alice
+        deal(token, ALICE, amount);
+        vm.startPrank(ALICE);
+        // approve token for bank to be able to transfer token from alice
+        IERC20(token).approve(address(bank), amount);
+        bank.deposit(amount);
+        skip(1 days);
+        vm.expectRevert("insufficient balance");
+        bank.withdraw(amount + 1);
+        vm.stopPrank();
+    }
+
+    function test_withdrawLocked() public {
+        // todo: test me
+        uint256 amount = 1e18;
+        // add token to alice
+        deal(token, ALICE, amount);
+        vm.startPrank(ALICE);
+        // approve token for bank to be able to transfer token from alice
+        IERC20(token).approve(address(bank), amount);
+        bank.deposit(amount);
+        vm.expectRevert("locked");
+        bank.withdraw(amount);
+        vm.stopPrank();
+    }
+}
---
+++ test/solutions/SolutionUsdtBank.t.sol
@@ -0,0 +1,205 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Bank} from "../../src/Bank.sol";
+import {SafeBank} from "../../src/SafeBank.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract SolutionUsdtBankTest is Test {
+    using SafeERC20 for IERC20;
+
+    // NOTE: usdt has bad implementation so we need to use SafeERC20 to avoid revert
+    // Bank bank;
+    SafeBank bank;
+
+    address token;
+    address usdt = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+    address ALICE = address(0xA11CE);
+    address BOB = address(0xB0B);
+    address OWNER = address(0xBEEF);
+
+    function setUp() public {
+        token = usdt;
+        vm.startPrank(OWNER);
+        // bank = new Bank(token);
+        bank = new SafeBank(token);
+        bank.open();
+        vm.stopPrank();
+    }
+
+    // =========================== forked test ===========================
+    // note: to reduce time of test
+    // find rpc from https://chainlist.org/ and use it to fork chain
+    // - 1. run test in another terminal: forge test -vv --mc UsdtBankTest --mt testFunction --fork-url https://rpc.ankr.com/eth
+    function test_depositSimple() public {
+        uint256 amount = 100 * 1e6;
+        // add token to alice
+        // note: deal will edit the storage of token contract
+        deal(token, ALICE, amount);
+        // load state for testing
+        uint256 aliceBalBf = IERC20(token).balanceOf(ALICE);
+        uint256 bankBalBf = IERC20(token).balanceOf(address(bank));
+        uint256 aliceBankBalBf = bank.balances(ALICE);
+        // prank alice
+        vm.startPrank(ALICE);
+        // approve token for bank to be able to transfer token from alice
+        IERC20(token).forceApprove(address(bank), amount);
+        bank.deposit(amount);
+        vm.stopPrank();
+        // check the balance of bank is correct
+        assertEq(IERC20(token).balanceOf(address(bank)) - bankBalBf, amount, "invalid bank balance");
+        // check the balance of alice is correct
+        assertEq(aliceBalBf - IERC20(token).balanceOf(ALICE), amount, "invalid alice balance");
+        // check the alice's balance in bank is correct
+        assertEq(bank.balances(ALICE) - aliceBankBalBf, amount, "invalid bank balance");
+        // check the last deposit time of alice is correct
+        assertEq(bank.lastDepositTimestamps(ALICE), block.timestamp, "invalid last deposit time");
+    }
+
+    // =========================== fuzz test ===========================
+    function testFuzz_deposit(uint256 amount) public {
+        // note: using amount to test might be failed for some edge cases
+        // todo: uncomment the line below to test
+        amount = bound(amount, 1, bank.maxBalance() - IERC20(token).balanceOf(address(bank)));
+        deal(token, ALICE, amount);
+        // load state for testing
+        uint256 aliceBalBf = IERC20(token).balanceOf(ALICE);
+        uint256 bankBalBf = IERC20(token).balanceOf(address(bank));
+        uint256 aliceBankBalBf = bank.balances(ALICE);
+        // prank alice
+        vm.startPrank(ALICE);
+        // approve token for bank to be able to transfer token from alice
+        IERC20(token).forceApprove(address(bank), amount);
+        bank.deposit(amount);
+        vm.stopPrank();
+        // check the balance of bank is correct
+        assertEq(IERC20(token).balanceOf(address(bank)) - bankBalBf, amount, "invalid bank balance");
+        // check the balance of alice is correct
+        assertEq(aliceBalBf - IERC20(token).balanceOf(ALICE), amount, "invalid alice balance");
+        // check the alice's balance in bank is correct
+        assertEq(bank.balances(ALICE) - aliceBankBalBf, amount, "invalid bank balance");
+        // check the last deposit time of alice is correct
+        assertEq(bank.lastDepositTimestamps(ALICE), block.timestamp, "invalid last deposit time");
+    }
+
+    function test_fuzzWithdraw(uint256 amount, uint256 withdrawAmount) public {
+        amount = bound(amount, 1, bank.maxBalance() - IERC20(token).balanceOf(address(bank)));
+        deal(token, ALICE, amount);
+        // prank alice
+        vm.startPrank(ALICE);
+        // approve token for bank to be able to transfer token from alice
+        IERC20(token).forceApprove(address(bank), amount);
+        bank.deposit(amount);
+        vm.stopPrank();
+        // load state for testing
+        uint256 aliceBalBf = IERC20(token).balanceOf(ALICE);
+        uint256 bankBalBf = IERC20(token).balanceOf(address(bank));
+        uint256 aliceBankBalBf = bank.balances(ALICE);
+        withdrawAmount = bound(withdrawAmount, 1, bank.balances(ALICE));
+        // time skip
+        skip(1 days);
+        // prank alice
+        vm.startPrank(ALICE);
+        bank.withdraw(withdrawAmount);
+        vm.stopPrank();
+        // check the balance of bank is correct
+        assertEq(IERC20(token).balanceOf(ALICE) - aliceBalBf, withdrawAmount, "invalid alice balance");
+        assertEq(bankBalBf - IERC20(token).balanceOf(address(bank)), withdrawAmount, "invalid bank balance");
+        assertEq(aliceBankBalBf - bank.balances(ALICE), withdrawAmount, "invalid bank balance");
+    }
+
+    // =========================== senario test ===========================
+    // note: senario test also have advanced testing
+    // read more: https://book.getfoundry.sh/forge/invariant-testing
+    function test_twoUserDeposit() public {
+        // alice deposit
+        uint256 aliceAmount = 100 * 1e6;
+        deal(token, ALICE, aliceAmount);
+        // prank alice
+        vm.startPrank(ALICE);
+        // approve token for bank to be able to transfer token from alice
+        IERC20(token).forceApprove(address(bank), aliceAmount);
+        bank.deposit(aliceAmount);
+        vm.stopPrank();
+        // load state for testing
+        uint256 aliceBalBf = IERC20(token).balanceOf(ALICE);
+        uint256 bankBalBf = IERC20(token).balanceOf(address(bank));
+        uint256 aliceBankBalBf = bank.balances(ALICE);
+        uint256 bobBankBalBf = bank.balances(BOB);
+        uint256 aliceLastDepositTime = bank.lastDepositTimestamps(ALICE);
+        // bob deposit
+        uint256 bobAmount = 2 * 100 * 1e6;
+        deal(token, BOB, bobAmount);
+        uint256 bobBalBf = IERC20(token).balanceOf(BOB);
+        // prank bob
+        vm.startPrank(BOB);
+        // approve token for bank to be able to transfer token from bob
+        IERC20(token).forceApprove(address(bank), bobAmount);
+        bank.deposit(bobAmount);
+        vm.stopPrank();
+        // check the balance of bank is correct
+        assertEq(IERC20(token).balanceOf(address(bank)) - bankBalBf, bobAmount, "invalid bank balance");
+        // check the balance of bob is correct
+        assertEq(bobBalBf - IERC20(token).balanceOf(BOB), bobAmount, "invalid bob balance");
+        // check the bob's balance in bank is correct
+        assertEq(bank.balances(BOB) - bobBankBalBf, bobAmount, "invalid bank balance");
+        // check the last deposit time of bob is correct
+        assertEq(bank.lastDepositTimestamps(BOB), block.timestamp, "invalid last deposit time");
+        // check the balance of alice is not changed
+        assertEq(aliceBalBf - IERC20(token).balanceOf(ALICE), 0, "invalid alice balance");
+        // check the alice's balance in bank is not changed
+        assertEq(bank.balances(ALICE) - aliceBankBalBf, 0, "invalid bank balance");
+        // check the last deposit time of alice is not changed
+        assertEq(bank.lastDepositTimestamps(ALICE), aliceLastDepositTime, "invalid last deposit time");
+    }
+
+    function test_twoUserDepositAliceWithdraw() public {
+        // todo: test me
+        // alice deposit
+        uint256 aliceAmount = 100 * 1e6;
+        deal(token, ALICE, aliceAmount);
+        // prank alice
+        vm.startPrank(ALICE);
+        // approve token for bank to be able to transfer token from alice
+        IERC20(token).forceApprove(address(bank), aliceAmount);
+        bank.deposit(aliceAmount);
+        vm.stopPrank();
+        // bob deposit
+        uint256 bobAmount = 2 * 100 * 1e6;
+        deal(token, BOB, bobAmount);
+        // prank bob
+        vm.startPrank(BOB);
+        // approve token for bank to be able to transfer token from bob
+        IERC20(token).forceApprove(address(bank), bobAmount);
+        bank.deposit(bobAmount);
+        vm.stopPrank();
+        // load state for testing
+        uint256 aliceBalBf = IERC20(token).balanceOf(ALICE);
+        uint256 bankBalBf = IERC20(token).balanceOf(address(bank));
+        uint256 aliceBankBalBf = bank.balances(ALICE);
+        uint256 bobBankBalBf = bank.balances(BOB);
+        uint256 bobBalBf = IERC20(token).balanceOf(BOB);
+        // time skip
+        skip(1 days);
+        vm.startPrank(ALICE);
+        bank.withdraw(aliceAmount);
+        vm.stopPrank();
+        // check the balance of bank is correct
+        assertEq(bankBalBf - IERC20(token).balanceOf(address(bank)), aliceAmount, "invalid bank balance");
+        // check the balance of alice is correct
+        assertEq(IERC20(token).balanceOf(ALICE) - aliceBalBf, aliceAmount, "invalid alice balance");
+        // check the alice's balance in bank is correct
+        assertEq(aliceBankBalBf - bank.balances(ALICE), aliceAmount, "invalid bank balance");
+        // check that bob's balance is not affected
+        assertEq(bobBalBf, IERC20(token).balanceOf(BOB), "invalid bob balance");
+        // check that bob's balance in bank is not affected
+        assertEq(bobBankBalBf, bank.balances(BOB), "invalid bank balance");
+    }
+
+    // =========================== related known expliot test ===========================
+    // note: this is plus
+    // if we implement some smart contract that similar to other project which has known exploit
+    // we can test it to make sure that our contract is not vulnerable
+}
